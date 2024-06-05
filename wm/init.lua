452cfa8874b59f7f1a8f30e3f4517f2b383a9434
jae woong <yeongnampjw@gmail.com>
2024-06-05 11:59:15 +0900

    feat(hammerspoon): Update config format

--- wm/init.lua
+++ wm/init.lua
@@ -3,69 +3,70 @@
 -- ------------------------------------------------------------------------------------------------------------------ --
 -- Configuration for visual styles
 -- LuaFormatter off
-hs.hints.fontName = "Galmuri11" -- Font name for hints
-hs.hints.fontSize = 14 -- Font size for hints
-hs.hints.showTitleThresh = 0 -- Threshold for showing window titles in hints
-hs.hints.iconAlpha = 1 -- Icon transparency for hints
-hs.alert.defaultStyle.textFont = "Galmuri11" -- Default font for alerts
-hs.alert.defaultStyle.textColor = {white = 1, alpha = 1} -- Default text color for alerts
-hs.alert.defaultStyle.textSize = 14 -- Default text size for alerts
-hs.alert.defaultStyle.radius = 4 -- Default corner radius for alerts
-hs.alert.defaultStyle.padding = 20 -- Default padding for alerts
+hs.hints.fontName                 = "Galmuri11"
+hs.hints.fontSize                 = 14
+hs.hints.showTitleThresh          = 0
+hs.hints.iconAlpha                = 1
+hs.alert.defaultStyle.textFont    = "Galmuri11"
+hs.alert.defaultStyle.textColor   = {white = 1, alpha = 1}
+hs.alert.defaultStyle.textSize    = 14
+hs.alert.defaultStyle.radius      = 4
+hs.alert.defaultStyle.padding     = 20
 -- LuaFormatter on
 
 -- Display initial configuration message
-hs.alert.show("🔨 <Hammerspoon>\n다시 설정하는 중...") -- Alert to show on startup
+hs.alert.show("🔨 <Hammerspoon>\n다시 설정하는 중...")
 
 -- Configuration for language switching using the ESC key
-local inputEnglish = "com.apple.keylayout.ABC" -- Input source ID for English
-local escBind -- Variable to hold the ESC key binding
+local inputEnglish = "com.apple.keylayout.ABC"
+local escBind
 
 -- Function to switch to English input source and then send ESC key event
 local function switchToEnglish()
     if hs.keycodes.currentSourceID() ~= inputEnglish then
-        hs.keycodes.currentSourceID(inputEnglish) -- Switch to English input source if not already selected
+        hs.keycodes.currentSourceID(inputEnglish)
     end
-    escBind:disable() -- Temporarily disable ESC key binding to avoid recursive calls
-    hs.eventtap.keyStroke({}, 'escape') -- Send ESC key event
-    escBind:enable() -- Re-enable ESC key binding
+    escBind:disable()
+    hs.eventtap.keyStroke({}, 'escape')
+    escBind:enable()
 end
 
-escBind = hs.hotkey.new({}, 'escape', switchToEnglish):enable() -- Bind ESC key to switchToEnglish function
+escBind = hs.hotkey.new({}, 'escape', switchToEnglish):enable()
 
 -- Key bindings for window focus hints and other functions
-local shiftCtrl = {"shift", "ctrl"} -- Modifier keys for custom shortcuts
+local shiftCtrl = {"shift", "ctrl"}
 
 -- Function to bind a shortcut key to a function
 local function bindShortcut(key, func) hs.hotkey.bind(shiftCtrl, key, func) end
 
 -- Window focus hints configuration
-hs.hints.hintChars = {"A", "S", "D", "F", "J", "K", "L", ";"} -- Characters to use for window hints
-bindShortcut("a", hs.hints.windowHints) -- Bind Shift+Ctrl+A to show window hints
+hs.hints.hintChars = {"A", "S", "D", "F", "J", "K", "L", ";"}
+bindShortcut("a", hs.hints.windowHints)
 
 -- Foundation remapping for Capslock key
-local FRemap = require('foundation_remapping') -- Import foundation remapping module
-local remapper = FRemap.new() -- Create a new remapper instance
-remapper:remap('capslock', 'f18') -- Remap Capslock to F18
-remapper:register() -- Register the remapper
+-- LuaFormatter off
+local FRemap    = require('foundation_remapping')
+local remapper  = FRemap.new()
+-- LuaFormatter on
+
+remapper:remap('capslock', 'f18')
+remapper:register()
 
 -- Language indicator configuration
-local boxHeight = 1 -- Height of the indicator boxes
-local boxWidth = 1 -- Width of the indicator boxes
-local boxAlpha = 1 -- Transparency of the indicator boxes
-local boxes = {} -- Table to hold the indicator box objects
+-- LuaFormatter off
+local boxHeight   = 1
+local boxWidth    = 1
+local boxAlpha    = 1
+local boxes       = {}
+-- LuaFormatter on
 
 -- Configuration for different language indicators
+-- LuaFormatter off
 local languageConfig = {
-    ["com.apple.keylayout.ABC"] = {
-        name = "English", -- Language name
-        color = {hex = "#FFC600"} -- Indicator color for English
-    },
-    ["com.apple.inputmethod.Korean.2SetKorean"] = {
-        name = "Korean", -- Language name
-        color = {hex = "#000000"} -- Indicator color for Korean
-    }
+    ["com.apple.keylayout.ABC"] = { name = "English", color = {hex = "#FFC600"} },
+    ["com.apple.inputmethod.Korean.2SetKorean"] = { name = "Korean", color = {hex = "#000000"} }
 }
+-- LuaFormatter on
 
 -- Function to create a new drawing box
 local function newBox() return
@@ -78,26 +79,26 @@ end
 
 -- Function to draw a rectangle as an indicator box
 local function drawRectangle(targetDraw, x, y, width, height, fillColor)
-    targetDraw:setSize(hs.geometry.rect(x, y, width, height)) -- Set the size of the box
-    targetDraw:setTopLeft(hs.geometry.point(x, y)) -- Set the top-left position of the box
-    targetDraw:setFillColor(fillColor) -- Set the fill color of the box
-    targetDraw:setFill(true) -- Enable fill for the box
-    targetDraw:setAlpha(boxAlpha) -- Set the transparency of the box
-    targetDraw:setLevel(hs.drawing.windowLevels.overlay) -- Set the drawing level
-    targetDraw:setStroke(false) -- Disable stroke for the box
-    targetDraw:setBehavior(hs.drawing.windowBehaviors.canJoinAllSpaces) -- Set the window behavior
-    targetDraw:show() -- Show the box
+    targetDraw:setSize(hs.geometry.rect(x, y, width, height))
+    targetDraw:setTopLeft(hs.geometry.point(x, y))
+    targetDraw:setFillColor(fillColor)
+    targetDraw:setFill(true)
+    targetDraw:setAlpha(boxAlpha)
+    targetDraw:setLevel(hs.drawing.windowLevels.overlay)
+    targetDraw:setStroke(false)
+    targetDraw:setBehavior(hs.drawing.windowBehaviors.canJoinAllSpaces)
+    targetDraw:show()
 end
 
 -- Function to enable the language indicator
 local function enableShow(language)
-    if not language then return end -- Exit if no language is provided
-    resetBoxes() -- Reset existing boxes
+    if not language then return end
+    resetBoxes()
     hs.fnutils.each(hs.screen.allScreens(), function(screen)
         local frame = screen:fullFrame()
 
-        -- Top indicator box
         -- LuaFormatter off
+        -- Top indicator box
         local topBox = newBox()
         drawRectangle(topBox, frame.x, frame.y, frame.w, boxHeight, language.color)
         table.insert(boxes, topBox)
@@ -128,8 +129,11 @@ end
 
 -- Handle language change events
 hs.keycodes.inputSourceChanged(function()
-    local currentSourceID = hs.keycodes.currentSourceID()
-    local language = languageConfig[currentSourceID]
+    -- LuaFormatter off
+    local currentSourceID   = hs.keycodes.currentSourceID()
+    local language          = languageConfig[currentSourceID]
+    -- LuaFormatter on
+
     disableShow()
     enableShow(language)
 end)
